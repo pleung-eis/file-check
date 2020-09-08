e28e8c5bd9a1e7acfbf2b7dfa98827e756e9e90f
pleung-eis <pleung@eagleinvsys.com>
2020-09-08 16:50:18 -0400

    Initial

---
+++ src/main.ts
@@ -0,0 +1,53 @@
+import * as core from '@actions/core'
+import fs from 'fs'
+
+async function checkExistence(path: string): Promise<boolean> {
+  try {
+    await fs.promises.access(path)
+  } catch (error) {
+    return false
+  }
+  return true
+}
+
+async function run(): Promise<void> {
+  try {
+    const files: string = core.getInput('files', {required: true})
+    const failure: boolean =
+      (core.getInput('allow_failure') || 'false').toUpperCase() === 'TRUE'
+    const fileList: string[] = files
+      .split(',')
+      .map((item: string) => item.trim())
+//    const restrictlist: string[] = searchFiles
+//      .split(',')
+//      .map((item: string) => item.trim())
+    const missingFiles: string[] = []
+    const path = require('path')
+
+    // Check in parallel
+    await Promise.all(
+      fileList.map(async (file: string) => {
+        const isPresent = await checkExistence(file)
+        if (isPresent) && fileList.includes(path.extname(file)) {
+          missingFiles.push(file)
+        }
+      })
+    )
+
+    if (missingFiles.length > 0) {
+      if (failure) {
+        core.setFailed(`These files are prohibited: ${missingFiles.join(', ')}`)
+      } else {
+        core.info(`These files are prohibited: ${missingFiles.join(', ')}`)
+      }
+      core.setOutput('files_exists', 'false')
+    } else {
+      core.info('🎉 No prohibited file types found!')
+      core.setOutput('files_exists', 'true')
+    }
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
